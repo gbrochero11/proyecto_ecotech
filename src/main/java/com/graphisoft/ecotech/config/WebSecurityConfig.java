2b707e0aaec73c66ba048771298ca7036b19d3ac
Jeison Galeano <ecotechcol2023@gmail.com>
2023-03-26 14:57:22 -0500

    Json Galeano
    Cors Active

--- src/main/java/com/graphisoft/ecotech/config/WebSecurityConfig.java
+++ src/main/java/com/graphisoft/ecotech/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().and().csrf().disable()
                 .authorizeRequests().antMatchers("/user/authenticate", "/user/register").permitAll().
                 anyRequest().authenticated().and().
                 exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
